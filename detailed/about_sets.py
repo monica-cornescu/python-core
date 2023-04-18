306e494103c8cd44443b45400a9e0d4bbdbdb930
Monica Cornescu <cornescu.monica@gmail.com>
2023-04-18 20:42:16 +0200

    some changes to strings and new file about sets

---
+++ detailed/about_sets.py
@@ -0,0 +1,10 @@
+""" learn more and practice with sets
+Sets are unordered collections of unique elements: sets are mutable, but the elements in it are immutable
+"""
+
+# Create a set with set(), not with {}, from any iterable element; duplicate are discarded
+myset = set([44, 21, 7, 769, 56566, 21])
+
+if __name__ == "__main__":
+    print(myset)
+
--- detailed/about_strings.py
+++ detailed/about_strings.py
@@ -8,37 +8,54 @@
 import math
 import json
 
+# length of a string
+print(len("this is quite a long course"))
 
+# concatenation with + and augmented assignment might produce problems with temporaries for memory and allocations:
+print("un" + "forget" + "able")
+
+word = "inter"
+word += "change"
+word += "able"
+print(word)
+
+# Prefer concatenating strings with join
 colors_string = ";".join(["red", "blue", "green", "yellow"])
+print(colors_string)
 colors_list = colors_string.split(";")
+print(colors_list)
 bedrijf = "".join(["jouw", "ICT", "vacature"])
+print(bedrijf)
 
+# partitioning produces a tuple, so it's often used in conjunction with tuple unpacking
 words = "unforgetable".partition("forget")
+print(words)
 departure, separator, arrival = "London:Edinburgh".partition(":")
+print(departure, arrival)
 origin, _, destination = "Amsterdam-Paris".partition("-")   # _ is  for unused or dummy values; we don't want to capture the separator
+print(origin, destination)
+
+# only the first encounter 0f the separator is considered:
+s = "crocodile"
+print(s.partition("o"))
 
 stringformatting_1 = "My {0}'s birthday is {1}. {0} was born in {2}".format("mom", "21st of May", 1945)
+print(stringformatting_1)
 stringformatting_2 = "My height and weight are {} m and {} kg".format(1.81, 64)
+print(stringformatting_2)
 position = "Current position is {latitude} {longitude}".format(latitude="60N", longitude="5E")
+print(position)
 galactic_position = "Galactic position is x={pos[0]},  y={pos[1]}, z={pos[2]}".format(pos=[65.2, 25.1, 82.2])
+print(galactic_position)
 math_constants = "Math constants are pi={m.pi} and e={m.e}".format(m=math)
+print(math_constants)
 math_constants_3f = "Math constants are pi={m.pi:.3f} and {m.e:.3f}".format(m=math)
+print(math_constants_3f)
 
 value = 4 * 20
 forfstring = f'The results is {value}'   # value is evaluated and inserted at runtime
+print(forfstring)
 
 
 if __name__ == '__main__':
-    print(colors_string)
-    print(colors_list)
-    print(bedrijf)
-    print(words)
-    print(departure, arrival)
-    print(origin, destination)
-    print(stringformatting_1)
-    print(stringformatting_2)
-    print(position)
-    print(galactic_position)
-    print(math_constants)
-    print(math_constants_3f)
-    print(forfstring)
+    print("Practice more with string operations!")
