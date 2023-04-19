fab590684ba0f6db6f81df768e4388e811a966e4
Monica Cornescu <cornescu.monica@gmail.com>
2023-04-19 15:18:14 +0200

    handling exceptions iteration 2

--- detailed/exceptional.py
+++ detailed/exceptional.py
@@ -1,27 +1,26 @@
 """
-Practice with exceptions, raising, handling
+Practice with exceptions, handling exceptions
 """
 DIGIT_MAP = {'zero': '0', 'one': '1', 'two': '2', 'three': '3', 'four': '4', 'five': '5', 'six': '6', 'seven': '7',
              'eight': '8', 'nine': '9'}
 
 
 def convert(s):
+    """ converting a string to an integer"""
+    x = -1
     try:
         number = ''
         for token in s:
             number += DIGIT_MAP[token]
         x = int(number)
         print(f"Conversion succeeded! x = {x}")
-    except KeyError:
+    except (KeyError, TypeError): # exception handler takes a tuple of 2 exception types
         print("Conversion failed!")
-        x = -1
-    except TypeError:
-        print("Conversion failed!")
-        x = -1
     return x
 
 
 if __name__ == '__main__':
     print(convert("one three three seven"))
     print(convert("one three three seven".split()))
+    print(convert("twotwotwo".split()))
     print(convert(734))
